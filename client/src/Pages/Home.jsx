5d8ce42057f9857fc9c8267ef4ebfd8e222ee5d1
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-08-29 10:20:40 +0600

    removed from client side

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Carousel from "../Components/Carousel";
+import TabCategories from "../Components/TabCategories";
+
+const Home = () => {
+  return (
+    <div>
+      <Carousel />
+      {/* <TabCategories /> */}
+    </div>
+  );
+};
+
+export default Home;
