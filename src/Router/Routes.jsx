85a8613076727fcfcebb7387927fdccf71abea8c
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-08-29 09:16:05 +0600

    haveing issue

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
 import ErrorPage from "../Pages/ErrorPage";
 import Home from "../Pages/Home";
+import Login from "../Pages/Authentication/Login";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login />,
       },
     ],
   },
