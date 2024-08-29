905966fa9ab7d77bf106f328bf09335d94463d5d
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-08-29 09:01:59 +0600

    slide done

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
 import ErrorPage from "../Pages/ErrorPage";
+import Home from "../Pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 
