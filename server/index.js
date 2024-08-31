f2721a68e64f15774975bc938591c03db63aa66b
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-08-31 16:55:45 +0600

    middleware not created

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
@@ -16,8 +17,11 @@ app.use(
 );
 app.use(express.json());
 
-// soloSphere
-// RDZz9P33zW6hOWJI
+// custom middleware
+// const verifyToken = async (req, res, next) => {
+//   console.log("hello from middleware", req.query);
+//   next();
+// };
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n7e36sw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -64,6 +68,14 @@ async function run() {
         .send({ "cookie removed": true });
     });
 
+    // jobs by my posted jobs
+    app.get("/jobs/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { "buyer.email": email };
+      const result = await jobCollection.find(query).toArray();
+      res.send(result);
+    });
+
     // get data from the db and use it
     app.get("/job", async (req, res) => {
       const result = await jobCollection.find().toArray();
@@ -101,14 +113,6 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await jobCollection.deleteOne(query);
-      res.send(result);
-    });
-
-    // jobs
-    app.get("/jobs/:email", async (req, res) => {
-      const email = req.params.email;
-      const query = { "buyer.email": email };
-      const result = await jobCollection.find(query).toArray();
       res.send(result);
     });
 
